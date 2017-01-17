429c4c259e059274254da022a4975d782362d95c
Nathaniel Wesley Filardo <nwf@cs.jhu.edu>
2017-01-17 01:41:57 -0500

    util/diag: robustify

--- util/diag.lua
+++ util/diag.lua
@@ -1,12 +1,20 @@
--- DEPENDS: file, rtcfifo, node, wifi
-print('INFO:',string.format("major=%d minor=%d dev=%d chip=%d flash=%d fs=%d fm=%d fs=%d",node.info()))
-print('HEAP:', node.heap())
-print('WIFI:',wifi.getmode())
-print('MAC:',wifi.sta.getmac(), wifi.ap.getmac())
-print('HOST:',wifi.sta.gethostname())
-print('WSTA:',wifi.sta.getconfig())
-print('WAP:',wifi.ap.getconfig())
-print('IP:',wifi.sta.getip(), wifi.ap.getip())
-if rtcfifo.ready() ~= 0 then print('RTCF:',rtcfifo.count()) else print('RTCF:','NOT PREPARED') end
-print('FS:', file.fsinfo()); for k,v in pairs(file.list()) do print("",k,v) end
+-- SOFT DEPENDS: file, rtcfifo, node, wifi
+if node then
+  print('INFO:',string.format("major=%d minor=%d dev=%d chip=%d flash=%d fs=%d fm=%d fs=%d",node.info()))
+  print('HEAP:', node.heap())
+end
+if wifi then
+  print('WIFI:',wifi.getmode())
+  print('MAC:',wifi.sta.getmac(), wifi.ap.getmac())
+  print('HOST:',wifi.sta.gethostname())
+  print('WSTA:',wifi.sta.getconfig())
+  print('WAP:',wifi.ap.getconfig())
+  print('IP:',wifi.sta.getip(), wifi.ap.getip())
+end
+if rtcfifo then
+  if rtcfifo.ready() ~= 0 then print('RTCF:',rtcfifo.count()) else print('RTCF:','NOT PREPARED') end
+end
+if file then
+  print('FS:', file.fsinfo()); for k,v in pairs(file.list()) do print("",k,v) end
+end
 print('GLOBAL:'); for k,v in pairs(_G) do print("",k,v) end
