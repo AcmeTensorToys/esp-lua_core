8362d7bbab4d2556298bf817e874bb718236959b
Rachael <srbennett@gmail.com>
2017-04-30 14:32:54 -0700

    tries to implement rgb --> hsl -> rgb, but fails compiling for an index out of range exception

--- net/nwfnet-diag.lua
+++ net/nwfnet-diag.lua
@@ -6,7 +6,7 @@ return function(ena)
     nn.onnet["diag"] = function(e,...)
       if     e == "wstagoip" then local t = ... ; print(e,t.IP,t.netmask,t.gateway)
       elseif e == "wstaconn" then local t = ... ; print(e,t.SSID,t.BSSID,t.channel)
-      elseif e == "wstadscn" then local t = ... ; print(e,t.SSID,t.BSSID,t.reason)
+      elseif e == "wstadscn" then local t = ... ; --print(e,t.SSID,t.BSSID,t.reason)
       else print(e,...) end
     end
   else
