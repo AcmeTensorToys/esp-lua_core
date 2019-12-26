447db3e87e7204f021dc1f0d51a190c991b90999
Nathaniel Wesley Filardo <nwfilardo@gmail.com>
2019-12-26 00:39:16 +0000

    pcall sntp to dodge panics
    Since sntp is mostly an autonomic thing, it's perhaps fine to drop these
    on the floor.
    
    For example, we might have a sync in progress, which throws.

--- net/nwfnet-sntp.lua
+++ net/nwfnet-sntp.lua
@@ -1,5 +1,5 @@
 local function dosntp(server)
-  sntp.sync((require "nwfnet").sntp,
+  pcall(sntp.sync,(require "nwfnet").sntp,
      function(sec,usec,server) rtctime.set(sec,usec); (require"nwfnet"):runnet("sntpsync",sec,usec,server) end,
      function(err) (require"nwfnet"):runnet("sntperr",err) end
   )
